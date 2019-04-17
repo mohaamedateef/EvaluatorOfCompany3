55242f749a8193e46d4fdb31f10252cb78e4157a
mohaamedateef <mohamedatef290@gmail.com>
2019-04-17 14:31:00 +0200

    Post Formula to the database

---
+++ API/recommendationformula.php
@@ -0,0 +1,28 @@
+<?php
+	class recommendationformula{
+		private $conn;
+		private $table = 'recommendationformula';
+
+		public $formula;
+		public $companyname;
+		public $companyid;
+
+		public function __construct($db){
+			$this->conn = $db;
+		}
+
+		public function postformula(){
+			$query = "INSERT INTO ".$this->table." (fromula, companyname, companyid) VALUES ('".$this->formula."','".$this->companyname."','".$this->companyid."')";
+			$this->formula = htmlspecialchars(strip_tags($this->formula));
+			$this->companyname = htmlspecialchars(strip_tags($this->companyname));
+			$this->companyid = htmlspecialchars(strip_tags($this->companyid));
+			if (mysqli_query($this->conn,$query))
+			{
+				return true;
+			}
+			printf("Error: %s.\n",$stmt->error);
+			return false;
+		}
+	}
+
+?>
